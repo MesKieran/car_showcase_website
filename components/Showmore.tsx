2d3ce491731192a88da12ab7c24e27e7fd9e7810
MesKieran <979714336@qq.com>
2023-10-13 00:15:52 +0800

    server side finished

--- components/Showmore.tsx
+++ components/Showmore.tsx
@@ -1,26 +1,36 @@
-'use client'
-import { ShowMoreProps } from "@/types"
-import { useRouter } from "next/navigation"
-import { CustomButton } from "."
-import { updateSearchPrams } from "@/utils"
+"use client";
 
-const Showmore = ({ pageNumber, isNext, setLimit }:ShowMoreProps) => {
-    const handleNavigation= () => {
-        const newLimit = (pageNumber + 1)*10
-        setLimit(newLimit)
-    }
-    return (
+import { useRouter } from "next/navigation";
+
+import { ShowMoreProps } from "@types";
+import { updateSearchParams } from "@utils";
+import { CustomButton } from "@components";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+
+    // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+    
+    router.push(newPathname);
+  };
+
+  return (
     <div className="w-full flex-center gap-5 mt-10">
       {!isNext && (
         <CustomButton
-            title="Show More"
-            btnType="button"
-            containerStyles="bg-primary-blue rounded-full text-white"
-            handleClick={handleNavigation}
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
         />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Showmore
+export default ShowMore;
